6d8776c58471ddd035a8469a176bf23bbaf71b75
Lucas Trevisan <161855418+ltrevisanbi@users.noreply.github.com>
2026-06-13 23:01:27 -0300

    Actualizo diario para poder borrar registros

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="viaje-v5";
+const CACHE="viaje-v6";
 const ASSETS=["index.html","manifest.json","icon-192.png","icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{self.clients.claim()});
